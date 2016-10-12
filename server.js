96fe931047a1baa39b4e8609ed5daa3a4d04be53
Rendxx <rendxx@gmail.com>
2016-10-12 18:33:59 -0500

    Test react

--- server.js
+++ server.js
@@ -35,41 +35,41 @@ app.use(function(req, res, next) {
     next();
 });
 
-// app.get('/api/comments', function(req, res) {
-//   fs.readFile(COMMENTS_FILE, function(err, data) {
-//     if (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-//     res.json(JSON.parse(data));
-//   });
-// });
-//
-// app.post('/api/comments', function(req, res) {
-//   fs.readFile(COMMENTS_FILE, function(err, data) {
-//     if (err) {
-//       console.error(err);
-//       process.exit(1);
-//     }
-//     var comments = JSON.parse(data);
-//     // NOTE: In a real implementation, we would likely rely on a database or
-//     // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-//     // treat Date.now() as unique-enough for our purposes.
-//     var newComment = {
-//       id: Date.now(),
-//       author: req.body.author,
-//       text: req.body.text,
-//     };
-//     comments.push(newComment);
-//     fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-//       if (err) {
-//         console.error(err);
-//         process.exit(1);
-//       }
-//       res.json(comments);
-//     });
-//   });
-// });
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
 
 
 app.listen(app.get('port'), function() {
