f3c7f57d70c2fa00156ae28b4fdfade6c70b3973
Rendxx <rendxx@gmail.com>
2016-10-17 18:47:03 -0500

    seperate css

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var CommentBox = require('./CommentBox.js');
+var style = require('../less/main.less');
 
 ReactDOM.render(
   <CommentBox url="/api/comments" pollInterval={2000}/>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 var pathToReactDom = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var extractTextPlugin = new ExtractTextPlugin("../style/[name].css")
 
 module.exports = {
-    plugins: [commonsPlugin],
+    plugins: [
+      commonsPlugin,
+      extractTextPlugin
+    ],
     entry: {
-        index : './src/js/main.js'
+        index : './src/js/main'
     },
     output: {
         path: 'public/wwwroot/script',
@@ -19,7 +24,7 @@ module.exports = {
             {
               test: /\.js|jsx$/,
               exclude: /node_modules/,
-              loader: "babel",
+              loader: 'babel',
               query:
                 {
                   presets:['react']
@@ -28,12 +33,12 @@ module.exports = {
             {
               test: /\.css$/,
               exclude: /node_modules/,
-              loader: 'style!css'
+              loader: ExtractTextPlugin.extract("style-loader", "css-loader")
             },
             {
               test: /\.less$/,
               exclude: /node_modules/,
-              loader: 'style!css!less'
+              loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
             },
             {
               test: /\.(png|jpg)$/,
