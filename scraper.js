10ae0a36f6625756367b65caf1825bc0f1062ac7
Rendxx <rendxx@gmail.com>
2017-01-24 01:58:31 -0600

    Scrape squad data

---
+++ scraper.js
@@ -0,0 +1,146 @@
+"use strict"
+
+const jsonfile = require('jsonfile'),
+      cheerio = require("cheerio"),
+      req = require("tinyreq");
+
+// data ----------------------------------------------------------------
+var DATA_League = [
+    {
+        name: 'English Premier League',
+        url: 'http://www.footballsquads.co.uk/eng/2016-2017/faprem.htm'
+    },
+    {
+        name: 'Spanish LaLiga',
+        url: 'http://www.footballsquads.co.uk/spain/2016-2017/spalali.htm'
+    },
+    {
+        name: 'German Bundesliga',
+        url: 'http://www.footballsquads.co.uk/ger/2016-2017/gerbun.htm'
+    },
+    {
+        name: 'Italian Serie A',
+        url: 'http://www.footballsquads.co.uk/italy/2016-2017/seriea.htm'
+    },
+    {
+        name: 'French Ligue 1',
+        url: 'http://www.footballsquads.co.uk/france/2016-2017/fralig1.htm'
+    }
+];
+
+var DATA_Squad_pre = 'http://www.footballsquads.co.uk/eng/2016-2017/';
+
+// function ------------------------------------------------------------
+function parseLeagueInfo (idx, url, cb){
+    req(url, (err, body) => {
+        if (err) { return cb(err); }
+        let $ = cheerio.load(body);
+        let pageData = [];
+
+        var dat = $('#main').find('h5 a');
+        console.log(dat.length);
+        for (var i=0; i<dat.length; i++){
+            var d = $(dat[i]);
+            pageData[i] = {
+              name: d.text(),
+              url: d.attr('href')
+            };
+        }
+
+        cb(idx, pageData);
+    });
+};
+
+function parseSquadInfo (idx, idx2, url, cb){
+    req(DATA_Squad_pre+url, (err, body) => {
+        if (err) { return cb(err); }
+        let $ = cheerio.load(body);
+        let pageData = [];
+
+        var dat = $('#main table').find('tr');
+        for (var i=0; i<dat.length; i++){
+            var d = parsePlayerInfo($, $(dat[i]));
+            if (d===null) continue;
+            if (d===true) break;
+            pageData.push(d);
+        }
+
+        cb(idx, idx2, pageData);
+    });
+};
+
+function parsePlayerInfo ($, dat){
+    var td = dat.find('td');
+    if (dat.find('h4').length>0) return true;
+    if ($(td[0]).text().replace(/\r/g,'').replace(/\n/g,'').trim()=='Number') return null;
+    if ($(td[1]).text().replace(/\r/g,'').replace(/\n/g,'').trim()=='') return null;
+    return {
+        number: $(td[0]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        name: $(td[1]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        nat: $(td[2]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        pos: $(td[3]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        height: $(td[4]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        weight: $(td[5]).text().replace(/\r/g,'').replace(/\n/g,'').trim(),
+        dob: $(td[6]).text().replace(/\r/g,'').replace(/\n/g,'').trim()
+    };
+};
+
+function scrapeLeague(league){
+    var squadData = [];
+    var count = league.length;
+    for (var i=0;i<league.length;i++){
+        var item = league[i];
+        parseLeagueInfo(i, item.url, function(idx, dat){
+            squadData[idx]={
+              name: league[idx].name,
+              url: dat
+            }
+            count--;
+            console.log(squadData[idx].name);
+            //console.log(squadData[idx].url);
+
+            if (count===0){
+                scrapeSquad(squadData);
+            }
+        });
+    }
+};
+
+function scrapeSquad(squad){
+    //console.log(squad);
+    var info = [];
+    var count = 0;
+    for (var i=0;i<squad.length;i++){
+        count+=squad[i].url.length;
+        info[i]={
+            name: squad[i].name,
+            squad: []
+        }
+    }
+    for (var i=0;i<squad.length;i++){
+        for (var j=0;j<squad[i].url.length;j++){
+            var item = squad[i].url[j];
+            parseSquadInfo(i,j, item.url, function(idx1, idx2, dat){
+                info[idx1].squad[idx2]={
+                  name: squad[idx1].url[idx2].name,
+                  player: dat
+                }
+                count--;
+
+                if (count===0){
+                    saveFile(info);
+                }
+            });
+        }
+    }
+};
+
+var saveFile = function (dat){
+    for (var i=0;i<dat.length;i++){
+        jsonfile.writeFile('data/'+dat[i].name+'.json', dat[i].squad, function (err) {
+          console.error(err)
+        });
+    }
+};
+
+scrapeLeague(DATA_League);
